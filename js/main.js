ee85d33228644257f0a62d7e679167277f73e8e1
JustSpokenCodes <JustSpokenCodes@users.noreply.github.com>
2019-05-13 00:06:17 -0700

    almost done making the all clickable

--- js/main.js
+++ js/main.js
@@ -2,6 +2,20 @@ class Tab {
     constructor(tab){
         this.tab = tab;
         this.all = document.querySelector(`.all[data-tab= '${this.tab.detaset.tab}]`);
+        this.all = new Tab(this.all)
+        this.tab.addEventListener('click', 
+        this.handleClick.bind(this))
+    }
+    handleClick(){
+        this.all.toggleContent();
+    }
+}
+class All {
+    constructor(all){
+        this.contentTab = all;
+    }
+    toggleContent(){
+        this.contentTab.classList.toggle('')
     }
 }
 
