4effcac9f4ea5c794c820c3bf31769cc5337e510
JustSpokenCodes <JustSpokenCodes@users.noreply.github.com>
2019-05-12 23:46:37 -0700

    Making it Actually wrap on ALL in h.

---
+++ js/main.js
@@ -0,0 +1,12 @@
+class Tab {
+    constructor(tab){
+        this.tab = tab;
+        this.
+    }
+}
+
+const tabs = document.querySelectorAll('.tab')
+
+tabs.forEach(tab => {
+    new Tab(tab);
+})
