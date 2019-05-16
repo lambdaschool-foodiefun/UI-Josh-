84299674ff82369d5e4a186cc9378fe9de6a7b74
JustSpokenCodes <JustSpokenCodes@users.noreply.github.com>
2019-05-15 22:18:23 -0700

    try something diff cursor wise

--- js/main.js
+++ js/main.js
@@ -5,10 +5,20 @@
 class Link{
     constructor(link){
         this.link = link;
-        this.
+        this.tab = document.querySelector(`.tab[data-tab= '${this.link.dataset.tab}']`);
+        this.tab = new Tab(this.tab);
+        this.link.addEventListener('click', this.handleClick.bind(this))
+    }
+    handleClick(){
+        this.tab.toggleTab();
     }
 }
 
+class Tab {
+    constructor(tab){
+        this.
+    }
+}
 const links = document.querySelectorAll('.link')
 
 links.forEach(link =>{
